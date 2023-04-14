f3a8948f07d80138cf7b6daa3a2a79f3d3bce292
MatusMoura <matus.barreto@gmail.com>
2023-04-14 13:59:05 -0300

    primeiras reinderizações

--- src/matus/minigame/Game.java
+++ src/matus/minigame/Game.java
@@ -1,18 +1,20 @@
 package matus.minigame;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
-/*
- * Nessa classe temos a janela do nosso game com a 
+/*Nessa classe temos a janela do nosso game com a 
  * interface Runnable e a classe Canvas e a classe 
  * Canvas do pacote awt.
  * 
  * Depois temos um looping infinito onde vai ser reinderizado 
- * o nosso jogo para isso usaremos o metodo Thread
- */
+ * o nosso jogo para isso usaremos o metodo Thread */
+
 public class Game extends Canvas implements Runnable{
 	
 	//tamanho da tela
@@ -24,8 +26,37 @@ public class Game extends Canvas implements Runnable{
 		//Com a classe setPreferredSize eu defino .
 		//a tela com o tamanho das variaveis anteriores.
 		this.setPreferredSize(new Dimension(WIDTH , HEIGHT));
-	
 	}
+	
+	/*metodo onde acontece a logica da gameplay
+	 * cada loopin (fps) é um spin
+	 */
+	public void spin() {
+		
+	}
+	
+	//metodo onde será reinderizado os graficos do game
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		
+		if(bs == null) {
+			this.createBufferStrategy(3);//otimização grafica
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();//obijeto responsavel por reinderizar graficos
+		
+		//pintando toda a janela
+		g.setColor(Color.green);
+		g.fillRect(0, 0,HEIGHT, WIDTH);
+		
+		//nosso primeiro retangulo
+		g.setColor(Color.blue);
+		g.fillRect(5, 5,25, 25);
+		
+		bs.show();
+	}
+	
 	public static void main(String[] args) {
 		
 		/*De maneira recusiva instanciamos 
@@ -51,10 +82,18 @@ public class Game extends Canvas implements Runnable{
 	@Override
 	public void run() {
 		while(true) {
-		System.out.println("looping");	
+			spin();
+			render();
+			
+			//logica para que o jogo rode a 60fps
+			try {
+				Thread.sleep(1000/60);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 	}
-	
 	
 }
